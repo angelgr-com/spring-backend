e94d1493c434085859c8fb361c52223570106eee
Angel Garrigues <dev@angelgr.com>
2022-06-10 17:48:23 +0200

    feat: added RestaurantRepository

--- src/main/java/com/angelgr/bookingrestaurant/repositories/RestaurantRepository.java
+++ src/main/java/com/angelgr/bookingrestaurant/repositories/RestaurantRepository.java
@@ -1,5 +1,21 @@
 package com.angelgr.bookingrestaurant.repositories;
 
-public class RestaurantRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.angelgr.bookingrestaurant.entities.Restaurant;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+	Optional<Restaurant> findById(Long id);
+	
+	Optional<Restaurant> findByName(String nameRestaurant);
+	
+	//	JPA query equivalent to @Query(value="SELECT * FROM restaurant",nativeQuery = true)
+	@Query("SELECT REST FROM Restaurant REST") 
+	public List<Restaurant> findRestaurants();
 }
